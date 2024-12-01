a06a07840407155bdc4ab026eb6b8386d5caa7b6
Printan <printan.nun@dome.tu.ac.th>
2024-12-01 16:00:01 +0700

    Add toggle menu

--- future.js
+++ future.js
@@ -2,3 +2,7 @@
 function toggleText(card) {
     card.classList.toggle('clicked');
 }
+function toggleMenu(){
+    const menu  = document.querySelector("nav ul");
+    menu.classList.toggle("active");
+}
